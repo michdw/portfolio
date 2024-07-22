1155ef0272ab17fe24f82397d1cd6eca59d62567
michdw <michd.weaver@gmail.com>
2024-07-21 23:51:42 -0400

    updated with React projects, replaced sidebar w header

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-function shiftFrame(pos) {
-  $('.frameView:nth-of-type(' + pos + ')').on('click', function () {
-    $('html, body').animate({
-      scrollTop: $('.frame:nth-of-type(' + pos + ')').position().top
-    }, 150);
-  });
-}
-
-function shiftActive(pos) {
-  if ($(document).scrollTop() >= $('.frame:nth-of-type(' + pos + ')').position().top - 100) {
-    $('.frameView').removeClass('active');
-    $('.frameView:nth-of-type(' + pos + ')').addClass('active');
-  } else {
-    shiftActive(pos - 1);
-  }
-}
-
-
 $(document).ready(function () {
   $('.dimmer').hide();
   
@@ -29,16 +11,6 @@ $(document).ready(function () {
   }, function () {
     $(this).find('span').addClass('aside');
     $(this).find('.dimmer').fadeOut(200);
-  });
-  
-  var frameCt = $('.frame').length;
-
-  for (var i = 1; i <= frameCt; i++) {
-    shiftFrame(i);
-  }
-
-  $(document).on('scroll', function () {
-    shiftActive(frameCt);
   });
 
 });
